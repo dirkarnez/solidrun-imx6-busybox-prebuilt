9f3e1689ab248daf65bae735b2d4a3a67d576db0
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-04 20:29:46 +0800

    - update makebusybox.sh

--- makebusybox.sh
+++ makebusybox.sh
@@ -25,17 +25,17 @@ announce "Building busybox" && \
 make help && \
 make defconfig && \
 sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config && \
-cat .config && \
-make CROSS_COMPILE="arm-linux-gnueabihf-" LDFLAGS="--static" && \
+cat .config &&  \
+make CROSS_COMPILE="arm-linux-gnueabihf-" LDFLAGS="--static" -j 8 && \
 make CONFIG_PREFIX="$(pwd)/initramfs" install && \
 announce "busybox build appears to have been successful"  && \
 cd initramfs && \
 mkdir -p bin sbin etc proc sys dev usr/bin usr/sbin && \
 cp /workspace/init . && \
-chmod +x init && \
+chmod +x ./init && \
 ls -R && \
 rm linuxrc && \
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > /dist/init.cpio && \
+find . | cpio -o -H newc | gzip > /dist/initramfs.cpio.gz && \
 announce "cpio have been created successfully"
 # && \
 # announce "copying files" && \
