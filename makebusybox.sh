dff1f8ddfe87fdc5f6597d24a1c5d1f8ed45980c
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-05 21:34:13 +0800

    - uupdate makebusybox.sh

--- makebusybox.sh
+++ makebusybox.sh
@@ -34,6 +34,11 @@ mkdir -p bin sbin etc proc sys dev usr/bin usr/sbin && \
 cp /workspace/init . && \
 chmod +x ./init && \
 ls -R && \
+cd /workspace/busybox/initramfs/dev && \
+mknod console c 5 1 && \
+mknod null c 1 3 && \
+cd /workspace/busybox/initramfs && \
+ls -R && \
 find . | cpio -o -H newc | gzip > /dist/initramfs.cpio.gz && \
 announce "cpio have been created successfully"
 # && \
